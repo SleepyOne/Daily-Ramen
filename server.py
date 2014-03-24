81f8d6aec5438f8c49ca5b86bf23438d03ef1fe8
Emorelleum <emorelleum@gmail.com>
2014-03-24 23:12:19 +0000

    Page restructuring Now, search results yield a list of clickable links, which go to a page which will eventually display the relevant information Comment box for recipes added

--- server.py
+++ server.py
@@ -26,6 +26,25 @@ def login():
 def createAccount():
   return render_template('createAccount.html')
 
+@app.route('/search_recipe/<id>',methods = ['GET','POST'])
+def searchRecipe(id):
+  info = []
+  info.append(id)
+  info.append(id)
+  info.append(id)
+  print info
+  print info[0]
+  print info[1]
+  print info[2]
+  ingredients = []
+  comments = []
+  if request.method == 'POST':
+    print request.form['comment']
+    #do stuff if the user just posted a comment
+
+  #canComment is true if the user is logged in, and hence can post a comment
+  return render_template('search_recipe.html',info = info,ingredients = ingredients,comments = comments,canComment = True)
+
 @app.route('/underConstruction')
 def underConstruction():
   return render_template('underConstruction.html')
