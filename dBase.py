a38c7b46dc2c9dc5fd75b54edc620815e363bb7e
Emorelleum <emorelleum@gmail.com>
2014-04-01 14:27:28 +0000

    SQL-Injection Safegaurd Escaped all mysql strings to mitigate injection vulnerabilities

--- dBase.py
+++ dBase.py
@@ -20,7 +20,7 @@ def dBaseInit():
   cursor = database.cursor()
   if cursor is not None and database is not None:
     print 'in'
-    cursor.execute('Select * from ' + TABLE + ' where recipe_name Like "' + "%a%" + '";')
+    cursor.execute('Select * from ' + MySQLdb.escape_string(TABLE) + ' where recipe_name Like "' + "%a%" + '";')
     stu = cursor.fetchall()
     for x in stu:
       print x[1]
@@ -30,7 +30,7 @@ def dBaseInit():
 def queryName(searchTerm): #queries by exact name
   if initialized:
     global cursor
-    cursor.execute('Select * from ' + TABLE + ' where recipe_name like "' + searchTerm + '";')
+    cursor.execute('Select * from ' + MySQLdb.escape_string(TABLE) + ' where recipe_name like "' + MySQLdb.escape_string(searchTerm) + '";')
     return cursor.fetchall()
   else:
     print "Not logged in to database!\n"
@@ -38,7 +38,7 @@ def queryName(searchTerm): #queries by exact name
 def searchSimilarName(searchTerm): #queries by term is contained in recipe name
   if initialized:
     global cursor
-    cursor.execute('Select * from ' + TABLE + ' where recipe_name Like "%' + searchTerm + '%" and recipe_name not like "' + searchTerm + '";')
+    cursor.execute('Select * from ' + MySQLdb.escape_string(TABLE) + ' where recipe_name Like "%' + MySQLdb.escape_string(searchTerm) + '%" and recipe_name not like "' + MySQLdb.escape_string(searchTerm) + '";')
     return cursor.fetchall()
   else:
     print "Not logged in to database!\n"
@@ -46,8 +46,8 @@ def searchSimilarName(searchTerm): #queries by term is contained in recipe name
 def getRecipeInfo(searchID): #gets specific information from a given recipe id    
   global cursor
   if initialized:
-    print 'Select recipe_name, recipe_creator, recipe_instructions from recipes where recipe_id = ' + str(searchID) + ';'
-    cursor.execute('Select recipe_name, recipe_creator, recipe_instructions from recipes where recipe_id = ' + str(searchID) + ';')
+    print 'Select recipe_name, recipe_creator, recipe_instructions from recipes where recipe_id = ' + MySQLdb.escape_string(str(searchID)) + ';'
+    cursor.execute('Select recipe_name, recipe_creator, recipe_instructions from recipes where recipe_id = ' + MySQLdb.escape_string(str(searchID)) + ';')
     return cursor.fetchall()
   else:
     print "Not logged in to database!\n"
@@ -56,7 +56,7 @@ def getRecipeInfo(searchID): #gets specific information from a given recipe id
 def getIngredients(searchID):#gets the ingredients of a given recipe id
   global cursor
   if initialized:
-    cursor.execute('Select ing.ingredient_name from recipes rec inner join recipeIngredients ri inner join ingredients ing on rec.recipe_id = ri.recipe_id and ri.ingredient_id = ing.ingredient_id where rec.recipe_id = ' + str(searchID) + ';')
+    cursor.execute('Select ing.ingredient_name from recipes rec inner join recipeIngredients ri inner join ingredients ing on rec.recipe_id = ri.recipe_id and ri.ingredient_id = ing.ingredient_id where rec.recipe_id = ' + MySQLdb.escape_string(str(searchID)) + ';')
     return cursor.fetchall()
   else:
     print "Not logged in to database!\n"
@@ -64,7 +64,7 @@ def getIngredients(searchID):#gets the ingredients of a given recipe id
 def getComments(searchID):#gets the commentss of a given recipe id
   global cursor
   if initialized:
-    cursor.execute('Select cm.comment from recipes rec inner join comments cm on rec.recipe_id = cm.recipe_id where rec.recipe_id = ' + str(searchID) + ';')
+    cursor.execute('Select cm.comment from recipes rec inner join comments cm on rec.recipe_id = cm.recipe_id where rec.recipe_id = ' + MySQLdb.escape_string(str(searchID)) + ';')
     return cursor.fetchall()
   else:
     print "Not logged in to database!\n"
@@ -72,7 +72,7 @@ def getComments(searchID):#gets the commentss of a given recipe id
 def addComment(recipeID,text):#adds a comment to a given recipe id
   global cursor
   if initialized:
-    cursor.execute('insert into comments values (NULL, '+ text + ','+ str(searchID) + ');')
+    cursor.execute('insert into comments values (NULL, '+ MySQLdb.escape_string(text) + ','+ MySQLdb.escape_string(str(searchID)) + ');')
     return cursor.fetchall()
   else:
     print "Not logged in to database!\n"    
@@ -80,7 +80,7 @@ def addComment(recipeID,text):#adds a comment to a given recipe id
 def searchIngredients(searchTerm): #queries by ingredients the recipe contains
   if initialized:
     global cursor
-    cursor.execute('Select * from ' + TABLE + ' JOIN ingredients ON ingredients.ingredient_id = recipeIngredients.ingredient_id JOIN recipeIngredients ON recipes.recipe_id = recipeIngredients.recipe_id WHERE ingredients.ingredient_name like "%' + searchTerm + '%;"')
+    cursor.execute('Select * from ' + MySQLdb.escape_string(TABLE) + ' JOIN ingredients ON ingredients.ingredient_id = recipeIngredients.ingredient_id JOIN recipeIngredients ON recipes.recipe_id = recipeIngredients.recipe_id WHERE ingredients.ingredient_name like "%' + MySQLdb.escape_string(searchTerm) + '%;"')
     return cursor.fetchall()
   else:
     print "Not logged in to database!\n"
